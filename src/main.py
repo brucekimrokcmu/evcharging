d1ab269ca560713d98c7d9b3b9afd1603b96f287
brucekimrokcmu <kwangkyk@alumni.cmu.edu>
2024-05-28 21:57:08 -0700

    Pass test on iksolver for qpos

--- src/main.py
+++ src/main.py
@@ -1,14 +1,10 @@
 import os
 import time
 import numpy as np
-from dm_control import mujoco
+from dm_control import mujoco as dm_mujoco
 from dm_control.mujoco.wrapper.mjbindings import mjlib, enums
-from dm_control import mjcf
-from dm_control import suite
-
 from dm_control.utils import inverse_kinematics as ik
-from dm_control import _render
-import mujoco.viewer as viewer
+import mujoco.viewer 
 
 # Set up the simulation parameters
 SIM_DURATION = 5.0
@@ -24,14 +20,13 @@ def get_random_target_pose():
 def linear_interpolate_planner(physics, site, target_pos, target_quat=None, num_of_interpolation=20, max_ik_attempts=10):
     if target_quat is None:
         target_quat = np.array([1, 0, 0, 0])
-    initial_xpos = physics.bind(site).xpos
+
+    initial_xpos = physics.data.xpos
     diff_xpos = target_pos - initial_xpos
 
     if num_of_interpolation < 2:
         raise ValueError("num_of_interpolation must be greater than 2")
     
-    physics.data.qpos[:] = physics.data.qpos.copy()
-
     for step in range(num_of_interpolation):
         interp_xpos = initial_xpos + diff_xpos * (step / (num_of_interpolation - 1))
 
@@ -46,18 +41,29 @@ def linear_interpolate_planner(physics, site, target_pos, target_quat=None, num_
 
 def main(): 
     curr_dir = os.path.dirname(os.path.abspath(__file__))
-    ur10e_model = os.path.join(curr_dir, '../data/universal_robots_ur10e/ur10e.xml')
-    physics = mujoco.Physics.from_xml_path(ur10e_model)
+    ur10e_model = os.path.join(curr_dir, '../data/universal_robots_ur10e/scene.xml')
+    physics = dm_mujoco.Physics.from_xml_path(ur10e_model)
+    model = physics.model.ptr
+    data = physics.data.ptr
 
-    target_pos, target_quat = get_random_target_pose()
+    initial_qpos = np.array([1.57, -0.78, -0.5, 0.5, 0.5, 0.5])
+    physics.data.qpos[:] = initial_qpos
+    physics.step()
 
-    ik_result = ik.qpos_from_site_pose(physics, 'attachment_site', target_pos, target_quat)
-    print(ik_result)
+    with mujoco.viewer.launch_passive(model=model, data=data,
+                                      show_left_ui=False, show_right_ui=False) as viewer:
+        viewer.cam.distance *= 2.5
 
-    # TODO: render the simulation
-    for _ in range(int(SIM_DURATION * 60)):
-        physics.step()
-        physics.render()
+        target_pos, target_quat = get_random_target_pose()
+        
+        print(physics.data.qpos)
+        # TODO figure out how to achieve target_quat
+        ik_result = ik.qpos_from_site_pose(physics, 'attachment_site', target_pos, target_quat=None)
+        print(ik_result)    
+        while viewer.is_running():
+
+            # linear_interpolate_planner(physics, 'attachment_site', target_pos, target_quat)
+            viewer.sync()
 
 if __name__ == "__main__":
     main()
