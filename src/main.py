ebd4addbffa6434db8ad76e4175c4171e96a32dc
brucekimrokcmu <kwangkyk@alumni.cmu.edu>
2024-05-30 21:50:34 -0700

    add test-ik

--- src/main.py
+++ src/main.py
@@ -10,34 +10,31 @@ import mujoco.viewer
 SIM_DURATION = 5.0
 # MODEL_PATH = 
 
-def get_random_target_pose():
+def get_random_target_pose(qpos_max, qpos_min, quat):
     target_pos = np.random.uniform(-0.5, 0.5, 3)
     target_quat = np.random.random(4)
     target_quat /= np.linalg.norm(target_quat)
     return target_pos, target_quat
 
 # TODO: implement set_site_to_xpos
-def linear_interpolate_planner(physics, site, target_pos, target_quat=None, num_of_interpolation=20, max_ik_attempts=10):
-    if target_quat is None:
-        target_quat = np.array([1, 0, 0, 0])
+def generate_trajectory(start_pos, start_quat, target_pos, target_quat, num_waypoints=1000):
+    trajectory = []
 
-    initial_xpos = physics.data.xpos
-    diff_xpos = target_pos - initial_xpos
+    return trajectory
 
-    if num_of_interpolation < 2:
-        raise ValueError("num_of_interpolation must be greater than 2")
+def test_ik(physics, target_pos=None, target_quat=None): 
     
-    for step in range(num_of_interpolation):
-        interp_xpos = initial_xpos + diff_xpos * (step / (num_of_interpolation - 1))
-
-        for attempt in range(max_ik_attempts):
-            ik_result = ik.qpos_from_site_pose(physics, site, interp_xpos, target_quat)
-            if ik_result.success:
-                physics.data.qpos[:] = ik_result.qpos
-                mjlib.mj_forward(physics.model, physics.data)
-                break
-            else:
-                print(f"Failed to find a solution at step {step} attempt {attempt}")
+    qpos = physics.data.qpos
+    xpos = physics.data.xpos
+    site_xpos = physics.data.site_xpos[-1,:]
+    print("Initial qpos: ", qpos)
+    print("Initial xpos: ", site_xpos)
+    if target_pos is None:
+        target_pos = site_xpos
+    
+    ik_result = ik.qpos_from_site_pose(physics, 'attachment_site', target_pos, target_quat=None)
+    print("IK result: ", ik_result)    
+    return ik_result
 
 def main(): 
     curr_dir = os.path.dirname(os.path.abspath(__file__))
@@ -46,24 +43,20 @@ def main():
     model = physics.model.ptr
     data = physics.data.ptr
 
-    initial_qpos = np.array([1.57, -0.78, -0.5, 0.5, 0.5, 0.5])
+    initial_qpos = np.array([1.57, -2.0, 1.3, 0.5, 0.5, 0.5])
     physics.data.qpos[:] = initial_qpos
-    physics.step()
 
-    with mujoco.viewer.launch_passive(model=model, data=data,
-                                      show_left_ui=False, show_right_ui=False) as viewer:
-        viewer.cam.distance *= 2.5
+    test_ik(physics)
 
-        target_pos, target_quat = get_random_target_pose()
-        
-        print(physics.data.qpos)
-        # TODO figure out how to achieve target_quat
-        ik_result = ik.qpos_from_site_pose(physics, 'attachment_site', target_pos, target_quat=None)
-        print(ik_result)    
-        while viewer.is_running():
 
-            # linear_interpolate_planner(physics, 'attachment_site', target_pos, target_quat)
-            viewer.sync()
+    # with mujoco.viewer.launch_passive(model=model, data=data,
+    #                                   show_left_ui=False, show_right_ui=False) as viewer:
+    #     viewer.cam.distance *= 2.5
+    #     target_pos, target_quat = get_random_target_pose()
+
+    #     while viewer.is_running():
+
+    #         viewer.sync()
 
 if __name__ == "__main__":
     main()
