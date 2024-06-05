6cc816eaa98f08ab6d71f68c61de1ae2e94a87fa
brucekimrokcmu <kwangkyk@alumni.cmu.edu>
2024-06-04 22:29:06 -0700

    draft trajectory generation and move to target - needs debugging & testing

--- src/main.py
+++ src/main.py
@@ -8,35 +8,45 @@ import mujoco.viewer
 
 # Set up the simulation parameters
 SIM_DURATION = 5.0
-# MODEL_PATH = 
 
-def get_random_target_pose(qpos_max, qpos_min, quat):
-    target_pos = np.random.uniform(-0.5, 0.5, 3)
-    target_quat = np.random.random(4)
-    target_quat /= np.linalg.norm(target_quat)
-    return target_pos, target_quat
+def get_random_pose(qpos_max=0.5, qpos_min=-0.5):
+    random_pos = np.random.uniform(qpos_min, qpos_max, 3)
+    random_quaternion = np.random.normal(0, 1, 4)
+    random_quaternion /= np.linalg.norm(random_quaternion)
+    random_pose = np.concatenate([random_pos, random_quaternion])
+    return random_pose
+
 
 # TODO slerp utility function for quaternion interpolation
 def slerp(q0, q1, t):
     pass
 
-def linear_trajectory_in_workspace(start_pos, end_pos, num_waypoints=100):
-    return np.linspace(start_pos, end_pos, num=num_waypoints)
+# TODO motion planner requires to follow along a linear trajectory in workspace
+def linear_trajectory_in_workspace(start_pose, end_pose, num_waypoints=100):
+    # return np.linspace(start_pos, end_pos, num=num_waypoints)
+    pass
 
-def solve_ik_for_waypoints(physics, waypoints, site_name='attachment_site'):
-    joint_poses = []
-    for waypoint in waypoints:
-        result = ik.qpos_from_site_pose(
-            physics,
-            site_name,
-            waypoint,
-            target_quat=None)  
-        if result.success:
-            joint_poses.append(result.qpos)
-        else:
-            print(f"IK failed at waypoint {waypoint}")
-            return None
-    return joint_poses
+def solve_ik_for_endpoints(physics, start_pose, end_pose, site_name='attachment_site'):
+    
+    start_xpos, start_quat = start_pose[:3], start_pose[3:]    
+    start_result = ik.qpos_from_site_pose(
+        physics,
+        site_name,
+        start_xpos,
+        start_quat=start_quat)
+    
+    assert start_result.success, "IK failed for start pose."
+
+    end_xpos, end_quat = end_pose[:3], end_pose[3:]
+    end_result = ik.qpos_from_site_pose(
+        physics,
+        site_name,
+        end_xpos,
+        target_quat=end_quat)
+    
+    assert end_result.success, "IK failed for end pose."
+    
+    return start_result.qpos, end_result.qpos
 
 def cubic_polynomial_interpolation(q0, qf, v0=None, vf=None, num_points=100, duration=2.0):
     if v0 is None:
@@ -59,29 +69,37 @@ def cubic_polynomial_interpolation(q0, qf, v0=None, vf=None, num_points=100, dur
     q_des = np.einsum('i,ij->ij', t**3, c[0]) + \
             np.einsum('i,ij->ij', t**2, c[1]) + \
             np.einsum('i,ij->ij', t, c[2]) + c[3]
-    
+        
     return q_des
 
-def generate_trajectory(physics, start_pos, end_pos, num_waypoints=100, duration=2.0, site_name='attachment_site'):
-    workspace_trajectory = linear_trajectory_in_workspace(start_pos, end_pos, num_waypoints)
+def generate_trajectory(physics, start_pose, end_pose, num_waypoints=100, duration=2.0, site_name='attachment_site'):    
+    """
+    start_pose: np.array([x, y, z, qx, qy, qz, qw])
+    end_pose: np.array([x, y, z, qx, qy, qz, qw])
+    """
+    start_qpos, end_qpos = solve_ik_for_endpoints(
+        physics, 
+        start_pose, end_pose, 
+        site_name)
     
-    joint_poses = solve_ik_for_waypoints(physics, [start_pos, end_pos], site_name)
-    if joint_poses is None:
-        print("IK failed for start or end pose.")
-        return None
+    assert start_qpos is not None, "IK failed for start pose."
+    assert end_qpos is not None, "IK failed for end pose."
     
-    q0, qf = joint_poses[0], joint_poses[-1]
-    v0, vf = np.zeros_like(q0), np.zeros_like(qf)    
-    trajectory = cubic_polynomial_interpolation(q0, qf, v0, vf, num_points=num_waypoints, duration=duration)
+    q0, qf = start_qpos, end_qpos
+    v0 = np.zeros_like(q0) # set initial velocity to zero
+    vf = np.zeros_like(qf) # set final velocity to zero
     
-    return trajectory, workspace_trajectory
-
+    joint_trajectory = cubic_polynomial_interpolation(
+        q0, qf, v0, vf, 
+        num_points=num_waypoints, 
+        duration=duration)
+    
+    return joint_trajectory
 
 def test_ik(physics, target_pos=None, target_quat=None): 
-    
     qpos = physics.data.qpos
-    xpos = physics.data.xpos
     site_xpos = physics.data.site_xpos[-1,:]
+    
     print("Initial qpos: ", qpos)
     print("Initial xpos: ", site_xpos)
     if target_pos is None:
@@ -89,7 +107,21 @@ def test_ik(physics, target_pos=None, target_quat=None):
     
     ik_result = ik.qpos_from_site_pose(physics, 'attachment_site', target_pos, target_quat=None)
     print("IK result: ", ik_result)    
-    return ik_result
+    return ik_result, ik_result.success
+
+def move_to_target(physics, target_pose, duration=2.0):
+    start_xpos = physics.data.site_xpos[-1,:]
+    start_xquat = physics.data.site_quat[-1,:] # TODO: check if this is correct
+    start_pose = np.concatenate([start_xpos, start_xquat])
+    print("Start pose: ", start_pose)
+    print("Target pose: ", target_pose)
+    joint_trajectory = generate_trajectory(physics, start_pose, target_pose, duration=duration)
+    
+    for qpos in joint_trajectory:
+        physics.data.qpos[:] = qpos
+        physics.step()
+        physics.render()
+        time.sleep(0.01)
 
 def main(): 
     curr_dir = os.path.dirname(os.path.abspath(__file__))
@@ -98,19 +130,8 @@ def main():
     model = physics.model.ptr
     data = physics.data.ptr
 
-    initial_qpos = np.array([1.57, -2.0, 1.3, 0.5, 0.5, 0.5])
-    physics.data.qpos[:] = initial_qpos
-
-    test_ik(physics)
-
-    # with mujoco.viewer.launch_passive(model=model, data=data,
-    #                                   show_left_ui=False, show_right_ui=False) as viewer:
-    #     viewer.cam.distance *= 2.5
-    #     target_pos, target_quat = get_random_target_pose()
-
-    #     while viewer.is_running():
-
-    #         viewer.sync()
+    target_pose = get_random_pose() 
+    move_to_target(physics, target_pose)
 
 if __name__ == "__main__":
     main()
