e1324333eb5adc2f9a5a2fac5f22557729c541d6
brucekimrokcmu <kwangkyk@alumni.cmu.edu>
2024-07-14 22:36:45 -0700

    draft residual observer

--- src/torque_reader.py
+++ src/torque_reader.py
@@ -3,40 +3,57 @@ from dm_control import mujoco as dm_mujoco
 import numpy as np
 
 class TorqueReader:
-    def __init__(self, model_path):
+    def __init__(self, model_path, config):
         self.physics = dm_mujoco.Physics.from_xml_path(model_path)
         self.model = self.physics.model.ptr
         self.data = self.physics.data.ptr
-        self.nv = self.model.nv
+        self.num_joints = self.model.nv
 
-    def reset(self):
-        mujoco.mj_resetData(self.model, self.data)
+        self.config = config
+        self.step_size = self.config['step_size']
+        self.step_count = 0
+        self.initialize_residual_observer()
+
+    def initilize_residual_observer(self):
+        self.gain_matirx = np.diag(self.config['diagonal_gain'] * self.num_joints)
+        self.residual = np.zeros(self.num_joints)
+        self.integral = np.zeros(self.num_joints)
+        self.prev_time = 0
 
     def step(self):
         mujoco.mj_step(self.model, self.data)
     
-    def get_joint_torques(self):
-        mujoco.mj_inverse(self.model, self.data)
+    def estimate_external_torques(self):
+        """
+        Estimate external torques using residual observer method.
+        Requires only proprioceptive measures (q, q_dot) and current commanded input u.
+        """
+        dt = self._calcualte_time_step()
+        u = self.data.ctrl[:self.num_joints]
 
-        # Get different torque components
-        qfrc_inverse = self.data.qfrc_inverse.copy()  # Main output of inverse dynamics
-        qfrc_applied = self.data.qfrc_applied.copy()  # Applied forces
-        qfrc_actuator = self.data.actuator_force.copy()  # Actuator forces
+        alpha = self._compute_dynamic_effects()
+        p = self._compute_generalized_momentum()
 
-        # Compute Jacobian'*xfrc_applied
-        jacp = np.zeros((3 * self.model.nbody, self.model.nv))
-        jacr = np.zeros((3 * self.model.nbody, self.model.nv))
-        mujoco.mj_jacBody(self.model, self.data, jacp, jacr, -1)
-        xfrc_applied = self.data.xfrc_applied.reshape(-1)
-        jac_xfrc = np.dot(jacp.T, xfrc_applied[:self.model.nbody*3]) + np.dot(jacr.T, xfrc_applied[self.model.nbody*3:])
+        self.integral += (alpha - u - self.residual) * dt
+        self.residual = self.gain_matirx @ (self.integral + p)
 
-        # Compute external forces (any unexplained force)
-        qfrc_external = qfrc_inverse - qfrc_applied - jac_xfrc - qfrc_actuator
+        estimated_torques = self.residual / dt
 
-        return {
-            'inverse': qfrc_inverse,
-            'applied': qfrc_applied,
-            'actuator': qfrc_actuator,
-            'jac_xfrc': jac_xfrc,
-            'external': qfrc_external
-        }
+        return estimated_torques
+    
+    def _calculate_time_step(self):
+        current_time = self.step_count * self.step_size
+        dt = current_time - self.prev_time
+        self.prev_time = current_time
+        return dt
+
+    def _compute_dynamic_effects(self):
+        dynamic_effects = self.data.qfrc_bais[:self.num_joints]
+        return dynamic_effects
+
+    def _compute_generalized_momentum(self):
+        mujoco.mj_mulM(self.model, self.data, self.data.qM, self.data.qvel)
+        return self.data.qM[:self.num_joints]
+
+    # TODO: compare with below 
+    # data.joint("my_joint").qfrc_constraint + data.joint("my_joint").qfrc_smooth
