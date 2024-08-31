fix gravity term, commanded joint torque. TODO: fix numerical integraiton error

src/residual_observer.py
@@ -51,7 +51,8 @@ class ResidualObserver:
             return self.residual, self.integral
 
 
-        tau = self.data.qfrc_actuator
+        # tau = self.data.qfrc_actuator  
+        tau = self.data.actuator_force
         alpha = self._compute_alpha()
         p = self._compute_generalized_momentum()
 
@@ -66,14 +67,31 @@ class ResidualObserver:
         """
         alpha_i = g_i(q) - (1/2)q̇^T @ (∂M(q)/∂(q_i)) @ q̇, i = 1, 2, ... , n
         """
-        alpha = self.data.qfrc_bias
+        alpha = np.zeros(self.num_joints)
+        q = self.data.qpos
+        g = self._compute_gravity_term()
         dq = self.data.qvel
 
         for i in range(self.num_joints):
             dM_dqi = self._finite_difference_partial_M_qi(i)
-            alpha[i] -= 0.5 * dq.T @ dM_dqi @ dq
-
+            alpha[i] = g[i] - 0.5 * dq.T @ dM_dqi @ dq
+            
         return alpha
+
+    def _compute_gravity_term(self):
+        g = np.zeros(self.model.nv)
+        gravity = self.model.opt.gravity
+
+        jacp = np.zeros((3, self.model.nv))  # For translational Jacobian
+        jacr = np.zeros((3, self.model.nv))  # For rotational Jacobian (unused)
+        
+        for i in range(1, self.model.nbody):  # Start from 1 to skip the world body
+            mass = self.model.body_mass[i]
+            mujoco.mj_jacBodyCom(self.model, self.data, jacp, jacr, i)
+            force = mass * gravity
+            g += jacp.T @ force
+
+        return g
 
     # TODO: Look into MuJoCo's API on derivative funcs
     def _finite_difference_partial_M_qi(self, i, eps=1e-6):

src/test_PID_control.py
@@ -5,6 +5,7 @@ from trajectory_generator import generate_trajectory, get_random_pose
 from visualization import Visualization
 from residual_observer import ResidualObserver
 from dm_control import mujoco as dm_mujoco
+import numpy as np
 
 def main():
     curr_dir = os.path.dirname(os.path.abspath(__file__))
@@ -15,10 +16,13 @@ def main():
 
     controller = UR10eController(physics, config_path)
     
-    start_pose = get_random_pose()
+    # start_pose = get_random_pose()
+    # TODO: set a fixed start pose (p, q) 
+    start_pose = np.array([0.4, -0.2, 0.5, 0.707, 0.0, 0.707, 0.0])
     controller.init_pose(start_pose)
-
-    target_pose = get_random_pose()
+    # TODO: set a fixed end pose (p, q)
+    # target_pose = get_random_pose()
+    target_pose = np.array([0.6, 0.2, 0.5, 0.707, 0.0, 0.0, 0.707])
 
     duration = 5.0
     joint_trajectory = generate_trajectory(

src/test_residual_observer.py
@@ -17,8 +17,11 @@ def main():
 
     controller = UR10eController(physics, config_path)
     
-    start_pose = get_random_pose()
-    target_pose = get_random_pose()
+    # start_pose = get_random_pose()
+    # target_pose = get_random_pose()
+    start_pose = np.array([0.4, -0.2, 0.5, 0.707, 0.0, 0.707, 0.0])
+    target_pose = np.array([0.6, 0.2, 0.5, 0.707, 0.0, 0.0, 0.707])
+
     controller.init_pose(start_pose)
     
     duration = 5.0